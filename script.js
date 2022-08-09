a8139c22fe2e5a42eb1bb944fb0415a4112975ae
Abhishekjadhavv <abhishekjadhav4553@gmail.com>
2022-08-09 20:32:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const bar = document.querySelector(".bar img ");
+const menu_list = document.querySelector(".main-menu");
+bar.addEventListener("click",()=>{
+ menu_list.classList.toggle("left");
+});
