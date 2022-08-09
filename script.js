22d95d7228cf039ad72fe092d5d2e0084741b107
Abhishekjadhavv <abhishekjadhav4553@gmail.com>
2022-08-09 20:39:30 +0530

    Initial commit

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const menu_list = document.querySelector(".main-menu");
 bar.addEventListener("click",()=>{
  menu_list.classList.toggle("left");
 });
+
+window.onscroll = (() => {
+  menu_list.classList.remove('left');    
+})
